71609b5128adc2a56946f7c360c40f9bb526321f
Adam Smith <coloradoasmith@gmail.com>
2020-12-23 16:25:27 -0700

    using redux devtools and moved store into its own file, 6.9 complete. Nice

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-
-const store = createStore(anecdoteReducer)
+import React from "react";
+import ReactDOM from "react-dom";
+import store from "./store";
+import { Provider } from "react-redux";
+import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
