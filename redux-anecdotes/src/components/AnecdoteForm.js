d0d4a1b78a2a1a7bd9b4a8d33191c7c1c8f60a89
Adam Smith <coloradoasmith@gmail.com>
2020-12-30 11:40:21 -0700

    new anecdote uses async action creators, 6.16 complete

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,17 +2,16 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { notifyCreate, notifyRemove } from "../reducers/notificationReducer";
-import anecdoteService from "../services/anecdotes";
 
-const NewAnecdote = (props) => {
+const NewAnecdote = () => {
   const dispatch = useDispatch();
 
   const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    const newNote = await anecdoteService.createNew(content);
-    dispatch(createAnecdote(newNote));
+
+    dispatch(createAnecdote(content));
     dispatch(notifyCreate(content));
     setTimeout(() => {
       dispatch(notifyRemove());
