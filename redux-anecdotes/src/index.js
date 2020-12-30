90e9ece2775d7b052315901118382bd95e77cc51
Adam Smith <coloradoasmith@gmail.com>
2020-12-30 11:28:46 -0700

    uses thunk to initialize redux store asynchronously, 6.15 complete

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 import App from "./App";
-import anecdoteService from "./services/anecdotes";
-import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 
-anecdoteService.getAll().then((a) => store.dispatch(initializeAnecdotes(a)));
+// anecdoteService.getAll().then((a) => store.dispatch(initializeAnecdotes(a)));
 
 ReactDOM.render(
   <Provider store={store}>
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from "../services/anecdotes";
+
 // const anecdotesAtStart = [
 //   "If it hurts, do it more often",
 //   "Adding manpower to a late software project makes it later!",
@@ -60,10 +62,13 @@ export const vote4 = (id) => {
   };
 };
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: "INIT_ANECDOTES",
-    data: anecdotes,
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: "INIT_ANECDOTES",
+      data: anecdotes,
+    });
   };
 };
 
