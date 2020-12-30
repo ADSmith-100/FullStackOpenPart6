4a81f518bee3f84843fa434cfeadc6ccd4d18540
Adam Smith <coloradoasmith@gmail.com>
2020-12-29 20:02:44 -0700

    fetches anecodtes from backend using json-server, 6.13 complete

--- redux-anecdotes/src/components/AnecdoteForm.js
+++ redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,17 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { notifyCreate, notifyRemove } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const NewAnecdote = (props) => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
+    const newNote = await anecdoteService.createNew(content);
+    dispatch(createAnecdote(newNote));
     dispatch(notifyCreate(content));
     setTimeout(() => {
       dispatch(notifyRemove());
--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 import App from "./App";
+import anecdoteService from "./services/anecdotes";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
+
+anecdoteService.getAll().then((a) => store.dispatch(initializeAnecdotes(a)));
 
 ReactDOM.render(
   <Provider store={store}>
